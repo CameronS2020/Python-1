72da3c180772d7dc56097366fe36c79b01ca97a3
CameronS2020 <161506690+CameronS2020@users.noreply.github.com>
2024-09-04 00:11:24 -0500

    Add files via upload

---
+++ my_conversion.py
@@ -0,0 +1,21 @@
+#Converting kilograms to pounds
+
+kilogram = 1
+
+pounds = 2.20462
+
+kilograms1 = 22
+kilograms2 = 1.5
+kilograms3 = 7.2
+kilograms4 = 90
+
+pounds1 = kilograms1 * pounds
+pounds2 = kilograms2 * pounds
+pounds3 = kilograms3 * pounds
+pounds4 = kilograms4 * pounds
+
+
+print(f"{kilograms1} kilograms is equal to {pounds1:.2f} pounds")
+print(f"{kilograms2} kilograms is equal to {pounds2:.2f} pounds")
+print(f"{kilograms3} kilograms is equal to {pounds3:.2f} pounds")
+print(f"{kilograms4} kilograms is equal to {pounds4:.2f} pounds")
