33152dbbeb415a37638fb691cbdf174b8894587e
CameronS2020 <161506690+CameronS2020@users.noreply.github.com>
2024-09-04 13:41:59 -0500

    Delete my_comments.py

--- my_comments.py
+++
@@ -1,8 +0,0 @@
-tank_size = 15 # This means gas tank size, not tank like army tank. (I was confused for a bit)
-
-distance = 600 # Total distance the car can travel before running out of gas. 
-
-mpg = distance / tank_size # This formula works because if 15 gallons maxes out at 600 distance traveled, each gallon is used 15 times, leading to 600/15 for a single gallon
-# ToDo: simplify what I said above.
-print("Miles per gallon:", mpg) # The print function works :>
-
