a67da1ba4b4b823cc6d281c3bc564a51f3cb3e6a
CameronS2020 <161506690+CameronS2020@users.noreply.github.com>
2024-09-04 00:04:29 -0500

    Add files via upload

---
+++ my_area.py
@@ -0,0 +1,11 @@
+length = 19
+
+width = 21
+
+area = width * length
+
+print("The width of the rectangle is", width)
+
+print("The length of the rectangle is", length)
+
+print("The area of the rectangle with these dimensions is", area)
