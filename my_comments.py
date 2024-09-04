74988149f68298fa860698c177995d0678066251
CameronS2020 <161506690+CameronS2020@users.noreply.github.com>
2024-09-04 13:43:23 -0500

    Add files via upload

---
+++ my_comments.py
@@ -0,0 +1,23 @@
+tank_size = 15 # This means gas tank size, not tank like army tank. (I was confused for a bit)
+
+distance = 600 # Total distance the car can travel before running out of gas. 
+
+mpg = distance / tank_size 
+"""This formula works because if 15 gallons maxes out at 600 distance traveled,
+ each gallon is used 15 times, leading to 600/15 for a single gallon"""
+# TODO - simplify what I said above.
+print("Miles per gallon:", mpg) #The print function works :>
+
+
+"""
+Type the above code into Visual Studio Code
+Add a minimum of 4 comments to the above Python program to make it more understandable.
+"""
+
+# TODO = do this!
+
+# Send code with questions to teacher, and they will give back a video looking over the code and letting me know what needs to be edited and things I can do to improve my code.
+
+# Special github comments.
+
+print("Hello world!") # This is an inline comment.
