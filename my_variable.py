226c9f980d2fcbe6480d06285f4bc6028285348b
CameronS2020 <161506690+CameronS2020@users.noreply.github.com>
2024-09-03 23:31:12 -0500

    Add files via upload

---
+++ my_variable.py
@@ -0,0 +1,17 @@
+# working with variables.
+
+literal = 24
+
+Integer = 1009
+
+Float = 1.234566
+
+name = "Jimmothy"
+
+Boolean = True
+
+print ("The name is", name)
+print ("The literal is", literal)
+print ("The Integer is", Integer)
+print ("The float is", Float)
+print ("The boolean state is", Boolean)
