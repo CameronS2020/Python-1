c3c74fe1073177de180ff2a12d13f660ce0adab6
CameronS2020 <161506690+CameronS2020@users.noreply.github.com>
2024-09-20 13:54:27 -0700

    Add files via upload

---
+++ my_understanding.py
@@ -0,0 +1,20 @@
+
+
+#literal
+print ("literal:", 10)
+
+#Integer
+y = 52
+print("Integer:", y)
+
+#float
+z = 18.32
+print("Float:", z)
+
+#string
+name = "Cameron"
+print("String:", name)
+
+#Boolean
+is_active = False
+print("Boolean:", is_active)
