2e167f686a3417fbd4f754d9a6082b50862be6a0
CameronS2020 <161506690+CameronS2020@users.noreply.github.com>
2024-10-28 14:01:55 -0500

    Update my_organizing_functions.py

--- my_organizing_functions.py
+++ my_organizing_functions.py
@@ -1,3 +1,4 @@
+#struggled for a bit of this, but function defines how the penny is doubled
 def double_penny(days, amount=0.01):
  
     if days == 1:
@@ -6,7 +7,7 @@ def double_penny(days, amount=0.01):
         return double_penny(days - 1, amount * 2)
     
  
-
+#The doubling penny function doesn't have to be called at the base level, it can be called only from another function if needed, like here to apply the equation in the main function.
 def main():
      days = int(input("Enter the number of days: "))
      total_amount = double_penny(days)
